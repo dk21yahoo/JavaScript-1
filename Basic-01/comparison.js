ef1f31eb8e55022f9eeec7f46f8859d4f16c3bb3
Tosif Mansuri <tosifmansuri760@gmail.com>
2024-08-07 19:09:30 +0530

    06-08-2024 and 07-08-2024
    Loops: for, while, dowhile, forOf, forIn, ForEach

--- Basic-01/comparison.js
+++ Basic-01/comparison.js
@@ -9,7 +9,7 @@ console.log("02" > 1)
 
 //null comparison
 console.log(null == 0); //false because null is only loosely equal to undefined and not to any other value.
-console.log(null <= 0); //Thus, console.log(null <= 0) returns true because null is converted to 0, and 0 <= 0 is true.
+console.log(null <= 0); //True, console.log(null <= 0) returns true because null is converted to 0, and 0 <= 0 is true.
 console.log(null > 0);
 console.log(null >= 0);
 console.log(null == undefined);
