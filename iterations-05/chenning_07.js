7a4a807c6eeffa5b6f7d09756bf64a214e7ca0e8
Deepak Asawara <162804964+dk21yahoo@users.noreply.github.com>
2024-08-17 17:41:24 +0530

    Update chenning_07.js

--- iterations-05/chenning_07.js
+++ iterations-05/chenning_07.js
@@ -3,8 +3,6 @@ const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // const newNums = myNumers.map( (num) => { return num + 10})
 
 const newNums = myNumers
-                .map((num) => num * 10 )
-                .map( (num) => num + 1)
-                .filter( (num) => num >= 40)
+                .map((num) => >= 40)
 
 console.log(newNums);
